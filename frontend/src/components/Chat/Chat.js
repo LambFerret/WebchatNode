77c646cf14d700761b4c96fd1a905863b6bb6ad8
LambFerret <dignrjrl12@gmail.com>
2021-10-23 21:03:14 +0900

    server fixed

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,7 +1,7 @@
 import './Chat.css'
 import socketio from 'socket.io-client';
 import React, { useEffect, useState } from "react";
-var socket = socketio("http://http://34.64.136.26:3001/")
+var socket = socketio("http://34.64.136.26:3001/")
 
 function Chat() {
     const [msg, setMsg] = useState('');
@@ -41,43 +41,6 @@ function Chat() {
             </ul>
         </div>
     )
-
-    // <form id='form' onSubmit={emitMessage}>
-    //     <input id="input" autoComplete="off" on={handleInput}/><button>Send</button>
-    // </form>
-
-
-
-    //   var socket = socketio.connect('http://localhost:3001');
-    //   var messages = document.getElementById('messages');
-    //   var form = document.getElementById("form");
-    //   var input = document.getElementById("input");
-    //   console.log(form);
-    //   form.addEventListener('submit', (e) => {
-    //     console.log(e)
-    //     e.preventDefault();
-    //     if (input.value) {
-    //     console.log(input.value)
-    //       socket.emit('chat message', input.value);
-    //       input.value = '';
-    //     }
-    //   })
-    //   socket.on("chat message", (msg) => {
-    //     var item = document.createElement('li')
-    //     item.textContent = msg
-    //     messages.appendChild(item)
-    //     window.scrollTo(0, document.body.scrollHeight)
-    //   })
-    //     return (
-
-    //   <body>
-    //     <ul id="messages"></ul>
-    // <form id="form" action="">
-    //   <input id="input" autoComplete="off" /><button>Send</button>
-    // </form>
-    // <script src="/socket.io/socket.io.js"></script>
-    //   </body>
-    //     )
 }
 
 export default Chat;
