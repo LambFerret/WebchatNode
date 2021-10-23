d23f9d00bef08885a7ac72578d3eb3d7d43b3583
LambFerret <dignrjrl12@gmail.com>
2021-10-23 19:07:20 +0900

    실시간 채팅 완성

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,31 +1,50 @@
 import './Chat.css'
 import socketio from 'socket.io-client';
 import React, { useEffect, useState } from "react";
+var socket = socketio("http://localhost:3001/")
 
 function Chat() {
-    const [res, setRes] = useState("");
-    useEffect(()=>{
-        const socket = socketio("http://localhost:3001/")
-        socket.on("chat message", data=>{
-            setRes(data)
-        })
+    const [msg, setMsg] = useState('');
+    const [chat, setChat] = useState([]);
 
-    }, [])
+    useEffect(() => {
+        socket.once("chat message", (msg) => {
+            setChat([...chat, msg])
+        })
+    }, [chat])
+
+    const renderChat = () => {
+        return chat.map((number,index) => <div key={index}><li >{number}</li></div>)
+    }
+
+    const onTextChange = e => {
+        setMsg(e.target.value)
+    }
+
+    const onMessageSubmit = (e) => {
+        e.preventDefault()
+        socket.emit("chat message", msg)
+        setMsg("")
+    }
 
     return (
         <div>
             <ul id="messages">
-                <li value={res}></li>
-                <form id='form'>
-                <input id="input"/><button>Send</button>
+                <form id='form' onSubmit={onMessageSubmit}>
+                    <input id='input' value={msg} onChange={onTextChange} />
+                    <button>SendPlz</button>
                 </form>
+                <div className="render-chat">
+                    <h1>render chat</h1>
+                    {renderChat()}
+                </div>
             </ul>
         </div>
     )
 
-            // <form id='form' onSubmit={emitMessage}>
-            //     <input id="input" autoComplete="off" on={handleInput}/><button>Send</button>
-            // </form>
+    // <form id='form' onSubmit={emitMessage}>
+    //     <input id="input" autoComplete="off" on={handleInput}/><button>Send</button>
+    // </form>
 
 
 
