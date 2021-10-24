385d08a203ba9b512b1b8c5d4665cda7156aca54
LambFerret <dignrjrl12@gmail.com>
2021-10-24 22:40:45 +0900

    update scroll function

--- frontend/src/components/Chat/Chat.js
+++ frontend/src/components/Chat/Chat.js
@@ -1,20 +1,23 @@
 import './Chat.css'
 import socketio from 'socket.io-client';
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 var socket = socketio("http://34.64.136.26:3001/")
 
 function Chat() {
     const [msg, setMsg] = useState('');
     const [chat, setChat] = useState([]);
+    const myRef = useRef(null)
 
     useEffect(() => {
         socket.once("chat message", (msg) => {
             setChat([...chat, msg])
+            myRef.current.scrollIntoView({ behavior: "smooth" })
         })
     }, [chat])
 
+
     const renderChat = () => {
-        return chat.map((number,index) => <div key={index}><li >{number}</li></div>)
+        return chat.map((number, index) => <div key={index}><li >{number}</li></div>)
     }
 
     const onTextChange = e => {
@@ -37,6 +40,7 @@ function Chat() {
                 <div className="render-chat">
                     <h1>render chat</h1>
                     {renderChat()}
+                <p ref={myRef}></p>
                 </div>
             </ul>
         </div>
